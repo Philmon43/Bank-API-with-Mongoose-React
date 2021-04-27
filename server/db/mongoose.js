f20f974bdefde85825bb9a5c5a4748bb2d6d644e
philmon43 <philmon43@gmail.com>
2021-04-27 10:08:48 +0300

    heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
+const url = "mongodb://127.0.0.1:27017/bank-app"
+
+if (process.env.NODE_ENV === "production") {
+    url = process.env.MONGODB_URI
+}
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/bank-app", {
+mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
